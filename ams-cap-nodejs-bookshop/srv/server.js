bd93915676f357b0daee8e3b1f580685f366c9a9
Manuel Fink <manuel.fink@sap.com>
2025-03-25 17:18:26 +0100

    adopt new API

--- ams-cap-nodejs-bookshop/srv/server.js
+++ ams-cap-nodejs-bookshop/srv/server.js
@@ -2,7 +2,7 @@ const cds = require('@sap/cds');
 const { cap: { amsPluginRuntime } } = require("@sap/ams");
 
 cds.on('bootstrap', () => {
-    const authorizationStrategy = amsPluginRuntime.authorizationStrategy;
-    authorizationStrategy.withApiMapper(api => `internal.${api}`);
-    // authorizationStrategy.withServicePlanMapper(plan => `internal.${plan}`);
+    const cdsAuthorizationStrategy = amsPluginRuntime.cdsAuthorizationStrategy;
+    cdsAuthorizationStrategy.xssecStrategy.withApiMapper(api => `internal.${api}`);
+    // cdsAuthorizationStrategy.xssecStrategy.withServicePlanMapper(plan => `internal.${plan}`);
 })
