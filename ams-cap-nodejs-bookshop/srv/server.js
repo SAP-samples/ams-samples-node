df9831ba154927e5958e032ce5a0e1426a27d4ab
Manuel Fink <manuel.fink@sap.com>
2025-03-26 11:45:50 +0100

    add AMS startup check

--- ams-cap-nodejs-bookshop/srv/server.js
+++ ams-cap-nodejs-bookshop/srv/server.js
@@ -6,3 +6,8 @@ cds.on('bootstrap', () => {
     cdsAuthorizationStrategy.xssecStrategy.withApiMapper(api => `internal.${api}`);
     // cdsAuthorizationStrategy.xssecStrategy.withServicePlanMapper(plan => `internal.${plan}`);
 })
+
+cds.on('served', async () => {
+    await amsPluginRuntime.getAms().whenReady(5000); // AMS startup check. TODO: couple this with /health endpoint instead
+    console.error("AMS ready");
+});
