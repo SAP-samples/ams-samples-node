e80add7c0d4e811b4b76f95f6495d689ac72ddb9
Manuel Fink <manuel.fink@sap.com>
2025-03-28 16:22:37 +0100

    refactor server.js

--- ams-cap-nodejs-bookshop/srv/server.js
+++ ams-cap-nodejs-bookshop/srv/server.js
@@ -1,26 +1,6 @@
 const cds = require('@sap/cds');
 const { technicalUserFlows: { TECHNICAL_USER, PRINCIPAL_PROPAGATION }, cap: { amsPluginRuntime } } = require("@sap/ams");
-
-const TECHNICAL_USER_APIS = [
-    "ReadCatalog"
-]
-
-const PRINCIPAL_PROPAGATION_APIS = [
-    "AMS_ValueHelp",
-    "ReadCatalog"
-]
-
-function mapTechnicalUserApi(api) {
-    if (TECHNICAL_USER_APIS.includes(api)) {
-        return `internal.${api}`;
-    }
-}
-
-function mapPrincipalPropagationApi(api) {
-    if (PRINCIPAL_PROPAGATION_APIS.includes(api)) {
-        return `internal.${api}`;
-    }
-}
+const { mapTechnicalUserApi, mapPrincipalPropagationApi } = require('../ams/mapAPI');
 
 cds.on('bootstrap', () => {
     const cdsAuthProvider = amsPluginRuntime.authProvider;
@@ -31,5 +11,5 @@ cds.on('bootstrap', () => {
 
 cds.on('served', async () => {
     await amsPluginRuntime.getAms().whenReady(5000); // AMS startup check. TODO: couple this with /health endpoint instead
-    console.error("AMS ready");
+    console.log("AMS has become ready.");
 });
