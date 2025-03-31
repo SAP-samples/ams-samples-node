3d14f47654c592bed2c6f8040f55b3404a58a47d
Manuel Fink <manuel.fink@sap.com>
2025-03-31 18:05:40 +0200

    update server.js to use amsCapPluginRuntime instead of amsPluginRuntime

--- ams-cap-nodejs-bookshop/srv/server.js
+++ ams-cap-nodejs-bookshop/srv/server.js
@@ -1,15 +1,15 @@
 const cds = require('@sap/cds');
-const { technicalUserFlows: { TECHNICAL_USER, PRINCIPAL_PROPAGATION }, cap: { amsPluginRuntime } } = require("@sap/ams");
+const { technicalUserFlows: { TECHNICAL_USER, PRINCIPAL_PROPAGATION }, amsCapPluginRuntime } = require("@sap/ams");
 const { mapTechnicalUserApi, mapPrincipalPropagationApi } = require('../ams/mapAPI');
 
 cds.on('bootstrap', () => {
-    const cdsAuthProvider = amsPluginRuntime.authProvider;
+    const cdsAuthProvider = amsCapPluginRuntime.authProvider;
     cdsAuthProvider.xssecAuthProvider.withApiMapper(mapTechnicalUserApi, TECHNICAL_USER);
     cdsAuthProvider.xssecAuthProvider.withApiMapper(mapPrincipalPropagationApi, PRINCIPAL_PROPAGATION);
     // cdsAuthProvider.xssecAuthProvider.withServicePlanMapper(plan => `internal.${plan}`);
 })
 
 cds.on('served', async () => {
-    await amsPluginRuntime.getAms().whenReady(5000); // AMS startup check. TODO: couple this with /health endpoint instead
+    await amsCapPluginRuntime.getAms().whenReady(5000); // AMS startup check. TODO: couple this with /health endpoint instead
     console.log("AMS has become ready.");
 });
