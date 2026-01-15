fbcde6f9d62ab3e53f3f38866f4e7cec26febc8e
Manuel Fink <manuel.fink@sap.com>
2026-01-15 16:50:14 +0100

    adapt tests

--- ams-cap-nodejs-bookshop/test/admin-service.test.js
+++ ams-cap-nodejs-bookshop/test/admin-service.test.js
@@ -3,27 +3,9 @@ const cds = require('@sap/cds')
 describe('AdminService', () => {
   const { GET, axios } = cds.test()
 
-  describe('called by alice (admin role mocked directly but not via policy)', () => {
+  describe('called by content-manager (cap.ContentManager policy assigned)', () => {
     beforeAll(() => {
-      axios.defaults.auth = { username: 'alice', password: '' }
-    })
-
-    it('/Books should return all Books because the admin role is not supplied via AMS, so the AMS attribute filters do not apply.', async () => {
-      const { status, data } = await GET `/admin/Books` 
-      expect(status).toBe(200)
-      expect(data.value?.length).toBe(5)
-    })
-
-    it('/Authors should return all Authors', async () => {
-      const { status, data } = await GET `/admin/Authors` 
-      expect(status).toBe(200)
-      expect(data.value?.length).toBe(4)
-    })
-  })   
-  
-  describe('called by bob (cap.admin policy assigned)', () => {
-    beforeAll(() => {
-      axios.defaults.auth = { username: 'bob', password: '' }
+      axios.defaults.auth = { username: 'content-manager', password: '' }
     })
 
     it('/Books should return all Books', async () => {
@@ -37,11 +19,50 @@ describe('AdminService', () => {
       expect(status).toBe(200)
       expect(data.value?.length).toBe(4)
     })
-  })  
-
-  describe('called by fred (no admin)', () => {
+  })   
+  
+  describe('called by stock-manager (cap.StockManager policy assigned)', () => {
     beforeAll(() => {
-      axios.defaults.auth = { username: 'fred', password: '' }
+      axios.defaults.auth = { username: 'stock-manager', password: '' }
+    })
+
+    it('/Books should return all Books', async () => {
+      const { status, data } = await GET `/admin/Books` 
+      expect(status).toBe(200)
+      expect(data.value?.length).toBe(5)
+    })
+
+    it('/Authors should return all Authors', async () => {
+      const { status, data } = await GET `/admin/Authors` 
+      expect(status).toBe(200)
+      expect(data.value?.length).toBe(4)
+    })
+  })
+
+  describe('called by stock-manager-fiction', () => {
+    beforeAll(() => {
+      axios.defaults.auth = { username: 'stock-manager-fiction', password: '' }
+    })
+
+    it('/Books should return 3 Books filtered by Mystery and Fantasy genres', async () => {
+      const { status, data } = await GET `/admin/Books` 
+      expect(status).toBe(200)
+      expect(data.value?.length).toBe(3)
+      expect(data.value).toContainEqual(expect.objectContaining({ title: 'Catweazle' }))
+      expect(data.value).toContainEqual(expect.objectContaining({ title: 'The Raven' }))
+      expect(data.value).toContainEqual(expect.objectContaining({ title: 'Eleonora' }))
+    })
+
+    it('/Authors should return all Authors', async () => {
+      const { status, data } = await GET `/admin/Authors` 
+      expect(status).toBe(200)
+      expect(data.value?.length).toBe(4)
+    })
+  })
+
+  describe('called by reader (no ManageAuthors or ManageBooks)', () => {
+    beforeAll(() => {
+      axios.defaults.auth = { username: 'reader', password: '' }
     })
 
     it('/Books should return status 403', async () => {
